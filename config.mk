3ddccf86d810df13b4ba11b19f62bdc4c8cc1fe6
Lucchetto <lucchetto.tie@live.com>
2019-10-15 20:00:12 +0200

    Bring back revengeos pie bootanimaton

--- config.mk
+++ config.mk
@@ -23,6 +23,17 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay/common/
 
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/revengeui/media/bootanimation_720.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/revengeui/media/bootanimation_1080.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/revengeui/media/bootanimation_1440.zip:system/media/bootanimation.zip
+else
+    $(error "RevengeOS: TARGET_BOOT_ANIMATION_RES is undefined")
+endif
+
 # Apps
 PRODUCT_PACKAGES += \
     ThemePicker
